4760ac75099071bfe9ebb8525572b9a7d56bfc7d
ile85 <ilco_dimeski@live.co.uk>
2024-05-05 14:27:42 +0000

    chore: Add dynamicEffects.js script and update layout.css for improved responsiveness and hover effects

--- assets/js/lib/particle-background.js
+++ assets/js/lib/particle-background.js
@@ -1,80 +1,91 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const isMobile = window.matchMedia("(max-width: 768px)").matches;
-  const particlesNumber = isMobile ? 50 : 100; // Adjust the number for density based on device type
-
-  if(document.getElementById('hero')) {
     particlesJS('hero', {
-      "particles": {
-          "number": {
-              "value": particlesNumber,
-              "density": {
-                  "enable": true,
-                  "value_area": 800
-              }
-          },
-          "color": {
-              "value": "#ffffff"
-          },
-          "shape": {
-              "type": "circle",
-          },
-          "opacity": {
-              "value": 0.5,
-              "random": false,
-          },
-          "size": {
-              "value": 3,
-              "random": true,
-          },
-          "line_linked": {
-              "enable": false,
-          },
-          "move": {
-              "speed": 1,
-              "direction": "none",
-              "random": false,
-              "straight": false,
-              "out_mode": "out",
-              "bounce": false,
-              "attract": {
-                  "enable": false,
-                  "rotateX": 600,
-                  "rotateY": 1200
-              }
-          }
-      },
-      "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-              "onhover": {
-                  "enable": true,
-                  "mode": "repulse"
-              },
-              "onclick": {
-                  "enable": true,
-                  "mode": "push"
-              }
-          },
-          "modes": {
-              "repulse": {
-                  "distance": 100,
-                  "duration": 0.4
-              },
-              "bubble": {
-                  "distance": 400,
-                  "size": 40,
-                  "duration": 2,
-                  "opacity": 8,
-                  "speed": 3
-              },
-              "push": {
-                  "particles_nb": 4
-              }
-          }
-      },
-      "retina_detect": true
+        particles: {
+            number: {
+                value: 80,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: '#ffffff'
+            },
+            shape: {
+                type: 'circle',
+                stroke: {
+                    width: 0,
+                    color: '#000000'
+                }
+            },
+            opacity: {
+                value: 0.5,
+                random: false
+            },
+            size: {
+                value: 3,
+                random: true
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                enable: true,
+                speed: 5,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out',
+                bounce: false,
+                attract: {
+                    enable: true,
+                    rotateX: 3000,
+                    rotateY: 3000
+                }
+            }
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: 'grab' // Changed from 'repulse' to 'grab'
+                },
+                onclick: {
+                    enable: true,
+                    mode: 'push'
+                },
+                resize: true
+            },
+            modes: {
+                grab: {
+                    distance: 200, // The radius in which the particles will move towards the cursor
+                    line_linked: {
+                        opacity: 0.8
+                    }
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4
+                },
+                push: {
+                    particles_nb: 4
+                },
+                remove: {
+                    particles_nb: 2
+                }
+            }
+        },
+        retina_detect: true
     });
-  } else {
-    console.error('The element with ID "hero" was not found.');
-  }
 });
